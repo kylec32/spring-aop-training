574c8aa72c4d3ca584134c77ba9f6ccda24bc590
Kyle Carter <kylec32@gmail.com>
2018-06-16 07:47:39 -0600

    Initial commit

---
+++ src/main/java/tk/lightweightcoding/aspects/AspectsApplication.java
@@ -0,0 +1,12 @@
+package tk.lightweightcoding.aspects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspectsApplication.class, args);
+	}
+}
